ee5a2cb991ae733d61ded115d5ca95be4ffb21b4
yaksh-shah2704 <yakshshah2704@gmail.com>
2025-06-09 19:21:47 +0530

    Cursor

---
+++ OOP/script.js
@@ -0,0 +1,138 @@
+// const student = {
+//     fullName: "Yaksh Shah",
+//     marks: 7.6,
+//     printMarks: function () {
+//         console.log("marks= ", this.marks);
+//     },
+// };
+
+// const employee = {
+//     calcTax() {
+//         console.log("tax rate is 10%.");
+//     },
+// };
+
+// const karanArjun = {
+//     salary: 50000,
+//     calcTax() {
+//         console.log("tax rate is 20%.");
+//     },
+// };
+
+// karanArjun.__proto__ = employee;
+
+
+// class ToyotaCar {
+//     constructor(brand, mileage) {
+//         console.log("Creating new object");
+//         this.brand = brand;
+//         this.mileage = mileage;
+//     }
+
+//     start() {
+//         console.log("start");
+//     }
+
+//     stop() {
+//         console.log("stop");
+//     }
+// }
+
+// let fortuner = new ToyotaCar("fortuner",10);
+// console.log(fortuner);
+// // fortuner.setBrand("fortuner");
+
+// let lexus = new ToyotaCar("lexus", 15);
+// console.log(lexus);
+// // lexus.setBrand("lexus");
+
+
+// class Parent {
+//     hello() {
+//         console.log("hello");
+//     }
+// }
+
+// class Child extends Parent {
+
+// }
+
+// let obj = new Child();
+
+
+// class Person {
+//     constructor(name) {
+//         this.species = "home sapiens";
+//         this.name = name;
+//     }
+//     eat() {
+//         console.log("eat");
+//     }
+// }
+
+// class Engineer extends Person {
+//     constructor(name) {
+//         super(name);
+//     }
+//     work() {
+//         console.log("Solve problem, build something");
+//     }
+// }
+// // let yakshObj = new Doctor();
+// let enjObj = new Engineer("Yaksh");
+
+
+// let DATA = "secret information";
+
+// class User {
+//     constructor(name, email) {
+//         this.name = name;
+//         this.email = email
+//     }
+
+//     viewData() {
+//         console.log("data = ", DATA);
+//     }
+// }
+
+// class Admin extends User {
+//     constructor(name, email) {
+//         super(name, email);
+//     }
+//     editDta() {
+//         DATA = "some new value";
+//     }
+// }
+
+// let student1 = new User("yaksh", "abc@email.com");
+// let student2 = new User("Dhruti", "dhruti@gmail.com");
+
+// let admin1 = new Admin("admin", "admin@gmail.com");
+
+
+let a = 5;
+let b = 10;
+
+
+    console.log("a = ", a);
+    console.log("b = ", b);
+    console.log("a+b = ", a + b);
+    console.log("a+b = ", a + b);
+    console.log("a+b = ", a + b);
+    console.log("a+b = ", a + b);
+    console.log("a+b = ", a + b);
+    console.log("a+b = ", a + b);
+    console.log("a+b = ", a + b);
+    console.log("a+b = ", a + b);
+    try {
+        console.log("a+b = ", a + c);
+    } catch (err) {
+        console.log(err);
+    }
+    console.log("a+b = ", a + b);
+    console.log("a+b = ", a + b);
+    console.log("a+b = ", a + b);
+    console.log("a+b = ", a + b);
+    console.log("a+b = ", a + b);
+    console.log("a+b = ", a + b);
+    console.log("a+b = ", a + b);
